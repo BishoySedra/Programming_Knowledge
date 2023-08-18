3187077c0bee09926627a7232eae0350326fae23
BishoySedra <bishosedra0@gmail.com>
2023-08-19 01:35:37 +0300

    -

--- JS/Regular Expressions/Regular_Expressions.js
+++ JS/Regular Expressions/Regular_Expressions.js
@@ -1,7 +1,7 @@
 let string = "12 13 20 50 126 454 8785"
 let arr = string.split(' ');
 arr = arr.filter((elem) => parseInt(elem) <= 50)
-console.log(arr);
+// console.log(arr);
 
 // ========================================
 // syntax => /pattern/modifiers or new RegExp(pattern, modifiers)
@@ -12,9 +12,17 @@ string = "Bishoy sedra bishoy"
 regex = /bishoy/ig
 regex = new RegExp("bishoy", "ig")
 
-console.log(string.match(regex));
+// console.log(string.match(regex));
 
 specialNums = "AbVdbQwbedj123!325wq@#$%!"
 regex = /[^a-z0-9]/gi
 
-console.log(specialNums.match(regex));
+// console.log(specialNums.match(regex));
+
+string = "1@3.org 1@@@@@3.com 1@k.com 1@3......com";
+regex = /\w@\w.(com|org)/g;
+// console.log(string.match(regex));
+
+string = "bsihoy bsihoyspam sspams spambishoy"
+regex = /(\bspam|spam\b)/g
+console.log(string.match(regex));
