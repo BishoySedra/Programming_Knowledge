0ed5dfc72a802e6d894b8cdc44da409956445396
Bishoy_Sedra <93890269+BishoySedra@users.noreply.github.com>
2023-12-15 04:31:25 +0200

    Files Modified

--- NodeJS/FreeCodeCamp Course/modules.js
+++
@@ -1,8 +0,0 @@
-const names = require('./names');
-const methods = require('./methods');
-
-require('./addFunction');
-
-methods.sayHi(names.bishoy);
-methods.sayHi(names.sedra);
-methods.sayHi(names.saber);
