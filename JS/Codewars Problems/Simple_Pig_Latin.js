ec971de93615fc8688fd3850095d139a4a652268
Bishoy_Sedra <93890269+BishoySedra@users.noreply.github.com>
2023-12-15 04:36:13 +0200

    Modified Files

--- JS/Codewars Problems/Simple_Pig_Latin.js
+++
@@ -1,9 +0,0 @@
-function pigIt(str) {
-    let re = /\w{1,}/ig;
-    str = str.replaceAll(re, function (match) {
-        return `${match.slice(1)}${match[0]}ay`;
-    });
-    return str;
-}
-
-console.log(pigIt('Pig latin is cool'));
--- NodeJS/Challenges/makingHttpCalls.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const page = require('./getFrontPageCode');
-
-const server = http.createServer((req, res) => {
-    page.getCode('http://www.google.com', req, res);
-});
-
-server.listen(5000);
-
