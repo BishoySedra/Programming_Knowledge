fa0025effad3e5fd76481eb2b2b554e25d46e8d8
Bishoy Sedra <bishosedra0@gmail.com>
2024-04-10 21:48:50 +0200

    Components Created!

---
+++ react_app/src/components/Expenses.js
@@ -0,0 +1,22 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./UI/Card";
+
+function Expenses(props) {
+  const expenses = props.items;
+
+  return (
+    <Card className="expenses">
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
